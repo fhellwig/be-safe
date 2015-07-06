ee38d7c6231aa03800cdf4a70151d728a35c7bfe
Brandt Heisey <heiseybj@state.gov>
2015-07-05 22:26:08 -0400

    fix deploy script to update beanstalk

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,10 +11,10 @@ zip $ARCHIVE_NAME start.sh Dockerrun.aws.json Dockerfile
 #DOCKERRUN_FILE=$CIRCLE_SHA1-Dockerrun.aws.json
 #sed "s/<TAG>/$CIRCLE_SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $ARCHIVE_NAME s3://elasticbeanstalk-us-east-1-143133207860/$ARCHIVE_NAME
-#aws elasticbeanstalk create-application-version --application-name RFQ-4QTFHS150004\
-#  --version-label $CIRCLE_SHA1 --source-bundle S3Bucket=elasticbeanstalk-us-east-1-143133207860,S3Key=$DOCKERRUN_FILE
+aws elasticbeanstalk create-application-version --application-name be-safe-docker\
+  --version-label $CIRCLE_SHA1 --source-bundle S3Bucket=elasticbeanstalk-us-east-1-143133207860,S3Key=$ARCHIVE_NAME
 
 # 
 # Update Elastic Beanstalk environment to new version
-#aws elasticbeanstalk update-environment --environment-name be-safe-docker \
-#    --version-label $CIRCLE_SHA1
+aws elasticbeanstalk update-environment --environment-name be-safe-docker \
+    --version-label $CIRCLE_SHA1
