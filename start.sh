e11f48bd7150a1294e0a12f579fe27f837d82fe1
Brandt Heisey <heiseybj@state.gov>
2015-07-05 23:59:55 -0400

    added new int tests,rm gulp from start

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #
 echo "starting the application set up...."
 cd /besafe
+
 echo "cleaning house..."
 rm -rf wwwroot/
+
 echo "getting most recent code..."
-git clone -b develop https://github.com/brandtheisey/be-safe.git wwwroot/
+git clone -b develop https://github.com/buchanan-edwards/be-safe.git wwwroot/
+
 cd wwwroot
-npm install -g gulp
 npm install
 
 echo "ok here we go..."
-gulp app
 nodejs server.js
